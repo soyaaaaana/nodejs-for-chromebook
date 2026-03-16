05540d9e1918af4eaa8417ff5c5307c2cf0b7fd5
そゃーな <148342996+soyaaaaana@users.noreply.github.com>
2026-03-16 19:24:23 +0900

    バグ修正
    3度目の正直

--- node_install.sh
+++ node_install.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 
-mkdir /usr/local/nodejs_for_chromebook/
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/nodejs_for_chromebook/
-curl -o /usr/local/nodejs_for_chromebook/installer https://github.com/soyaaaaana/nodejs-for-chromebook/releases/download/binary/node_install
-curl -o /usr/local/nodejs_for_chromebook/libatomic.so.1 https://raw.githubusercontent.com/soyaaaaana/nodejs-for-chromebook/main/library/libatomic.so.1
-curl -o /usr/local/nodejs_for_chromebook/libstdc++.so.6 https://raw.githubusercontent.com/soyaaaaana/nodejs-for-chromebook/main/library/libstdc++.so.6
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/tmp/nodejs_for_chromebook/
+curl -o /tmp/nodejs_for_chromebook/installer https://raw.githubusercontent.com/soyaaaaana/nodejs-for-chromebook/main/binary/node_install
+curl -o /tmp/nodejs_for_chromebook/libatomic.so.1 https://raw.githubusercontent.com/soyaaaaana/nodejs-for-chromebook/main/library/libatomic.so.1
+curl -o /tmp/nodejs_for_chromebook/libstdc++.so.6 https://raw.githubusercontent.com/soyaaaaana/nodejs-for-chromebook/main/library/libstdc++.so.6
 
-chmod a+rx /usr/local/nodejs_for_chromebook/installer
-/usr/local/nodejs_for_chromebook/installer
-
-rm -rf /usr/local/nodejs_for_chromebook/
+chmod a+rx /tmp/nodejs_for_chromebook/installer
+/tmp/nodejs_for_chromebook/installer

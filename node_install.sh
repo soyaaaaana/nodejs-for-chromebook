0fe2d6237fdee4ad11bd67eb1b2c601020b02e52
そゃーな <148342996+soyaaaaana@users.noreply.github.com>
2026-03-16 20:12:22 +0900

    バグ修正
    5度目の正直
    ライブラリの読み込みができない問題を修正

--- node_install.sh
+++ node_install.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-mkdir /usr/local/nodejs_for_chromebook/
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/nodejs_for_chromebook/
-curl -L -o /usr/local/nodejs_for_chromebook/installer https://github.com/soyaaaaana/nodejs-for-chromebook/releases/download/binary/node_install
-curl -L -o /usr/local/nodejs_for_chromebook/libatomic.so.1 https://raw.githubusercontent.com/soyaaaaana/nodejs-for-chromebook/main/library/libatomic.so.1
-curl -L -o /usr/local/nodejs_for_chromebook/libstdc++.so.6 https://raw.githubusercontent.com/soyaaaaana/nodejs-for-chromebook/main/library/libstdc++.so.6
+mkdir /usr/local/nodejs_for_chromebook/ 2>/dev/null
+curl -L -o /usr/local/nodejs_for_chromebook/installer https://github.com/soyaaaaana/nodejs-for-chromebook/releases/download/binary/node_install 2>/dev/null
+curl -L -o /usr/local/lib/libatomic.so.1 https://raw.githubusercontent.com/soyaaaaana/nodejs-for-chromebook/main/library/libatomic.so.1 2>/dev/null
+curl -L -o /usr/local/lib/libstdc++.so.6 https://raw.githubusercontent.com/soyaaaaana/nodejs-for-chromebook/main/library/libstdc++.so.6 2>/dev/null
 
 chmod a+rx /usr/local/nodejs_for_chromebook/installer
 /usr/local/nodejs_for_chromebook/installer

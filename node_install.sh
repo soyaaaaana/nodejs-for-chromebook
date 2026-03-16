4e1cc277e4eb0735fb25317d63beec3da12a302b
そゃーな <148342996+soyaaaaana@users.noreply.github.com>
2026-03-16 19:10:35 +0900

    バグ修正

--- node_install.sh
+++ node_install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+mkdir /tmp/nodejs_for_chromebook/
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/tmp/nodejs_for_chromebook/
 curl -o /tmp/nodejs_for_chromebook/installer https://github.com/soyaaaaana/nodejs-for-chromebook/releases/download/binary/node_install
 curl -o /tmp/nodejs_for_chromebook/libatomic.so.1 https://raw.githubusercontent.com/soyaaaaana/nodejs-for-chromebook/main/library/libatomic.so.1

c085e671378f141d836e2646d81237ca9c5a095d
そゃーな <148342996+soyaaaaana@users.noreply.github.com>
2026-05-05 23:43:50 +0900

    Add files via upload

--- node_install.sh
+++ node_install.sh
@@ -12,7 +12,7 @@ else
 fi
 
 mkdir /usr/local/nodejs_for_chromebook/
-curl -L -o /usr/local/nodejs_for_chromebook/installer https://github.com/soyaaaaana/nodejs-for-chromebook/releases/download/binary/node_install_${FILE_ARCH} 2>/dev/null
+curl -L -o /usr/local/nodejs_for_chromebook/installer https://github.com/soyaaaaana/nodejs-for-chromebook/releases/download/binary/node_install_${URL_ARCH} 2>/dev/null
 
 chmod a+rx /usr/local/nodejs_for_chromebook/installer
 /usr/local/nodejs_for_chromebook/installer

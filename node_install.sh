9961af1a6a1db4bb587770569f321c39bd8a55d3
そやーな <148342996+soyaaaaana@users.noreply.github.com>
2026-09-18 18:45:53 +0900

    Update node_install.sh

--- node_install.sh
+++ node_install.sh
@@ -13,6 +13,7 @@ fi
 
 mkdir /usr/local/nodejs_for_chromebook/ 2>/dev/null
 
+# binary_v2 ha bagutta
 curl -L -o /usr/local/nodejs_for_chromebook/installer https://github.com/soyaaaaana/nodejs-for-chromebook/releases/download/binary_v2/node_install_${URL_ARCH} 2>/dev/null
 
 chmod a+rx /usr/local/nodejs_for_chromebook/installer 2>/dev/null

89c123c039456b3d0d89dd73f339eae634170d0d
そやーな <148342996+soyaaaaana@users.noreply.github.com>
2026-09-18 18:12:13 +0900

    Update node_install.sh

--- node_install.sh
+++ node_install.sh
@@ -13,8 +13,7 @@ fi
 
 mkdir /usr/local/nodejs_for_chromebook/ 2>/dev/null
 
-# binary_v2 ha nazeka segment fault ni naru
-curl -L -o /usr/local/nodejs_for_chromebook/installer https://github.com/soyaaaaana/nodejs-for-chromebook/releases/download/binary/node_install_${URL_ARCH} 2>/dev/null
+curl -L -o /usr/local/nodejs_for_chromebook/installer https://github.com/soyaaaaana/nodejs-for-chromebook/releases/download/binary_v2/node_install_${URL_ARCH} 2>/dev/null
 
 chmod a+rx /usr/local/nodejs_for_chromebook/installer 2>/dev/null
 /usr/local/nodejs_for_chromebook/installer
